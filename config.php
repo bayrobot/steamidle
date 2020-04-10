cca751259702a189a57b75540fce9055c09a83ec
bayrobot <50464029+bayrobot@users.noreply.github.com>
2020-04-11 00:24:12 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+/* Define username and password. Goto http://www.passwordtool.hu/php5-password-hash-generator fo that. Default password is "xd"*/
+$Username = "palone";
+$Password = "$2y$10$.W9PMDwcIcKvi.lBKFc9UOwBt5Z8efiglTGt3K3uuJ.HPMcskvGcW";
